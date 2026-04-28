bb553d7f7d4ca63a1644d515b7590e072b28916f
swetha <swethakailasam1@gmail.com>
2026-04-29 00:19:08 +0530

    Added one project as submodule

--- src/main/java/com/microservice/accounts/models/BaseEntity.java
+++ src/main/java/com/microservice/accounts/models/BaseEntity.java
@@ -2,25 +2,34 @@ package com.microservice.accounts.models;
 
 import java.time.LocalDateTime;
 
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter@Setter@ToString
 public class BaseEntity {
 
+	@CreatedDate
 	@Column(updatable=false)
 	private LocalDateTime createdAt;
 	
+	@CreatedBy
 	@Column(updatable=false)
 	private String createdBy;
 	
+	@LastModifiedDate
 	@Column(insertable=false)
 	private LocalDateTime updatedAt;
 	
+	@LastModifiedBy
 	@Column(insertable=false)
 	private String updatedBy;
 }
--- src/main/java/com/microservice/accounts/service/impl/AccountsServiceImpl.java
+++ src/main/java/com/microservice/accounts/service/impl/AccountsServiceImpl.java
@@ -36,8 +36,7 @@ public class AccountsServiceImpl implements IAccountsService{
 			throw new CustomerAlreadyExistsException("Customer already registered with "
 					+ " the given mobile number "+customerDto.getMobileNumber());
 		}
-		customer.setCreatedAt(LocalDateTime.now());
-		customer.setCreatedBy("Anonymous");
+	
 		Customer savedCustomer=customerRepository.save(customer);
 		accountsRepository.save(createNewAccount(savedCustomer));
 		
@@ -49,8 +48,7 @@ public class AccountsServiceImpl implements IAccountsService{
 		newAccount.setAccountNumber(randomAccNumber);
 		newAccount.setAccountType(AccountsConstants.SAVINGS);
 		newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-		newAccount.setCreatedAt(LocalDateTime.now());
-		newAccount.setCreatedBy("Anonymous");
+		
 		return newAccount;
 	}
 	@Override
